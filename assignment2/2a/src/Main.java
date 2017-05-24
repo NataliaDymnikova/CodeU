d75ee169fd06d71cddaa05f599cb5bacb5e2b341
NataliaDymnikova <natalia.dymnikova@gmail.com>
2017-05-24 16:44:01 +0300

    Made first task of 2nd assignment

---
+++ assignment2/2a/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.Console;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Tree<Integer> tree = new Tree<Integer>(-2);
+        tree.add(-1, -2, false);
+
+        // tree:
+        //      -2
+        //     0  -1
+        //    2     1
+        //   4       3
+        //  6         5
+        // 8           7
+        //              9
+        for (int i = 0; i < 10; i++) {
+            boolean isLeft = i % 2 == 0;
+            tree.add(i, i - 2, isLeft);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int el = scanner.nextInt();
+
+        tree.getAncestors(el).forEach(i -> System.out.print(i.toString() + " "));
+    }
+}
---
+++ assignment2/2a/src/Tree.java
@@ -0,0 +1,84 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Tree for getting ancestors
+ */
+public class Tree<T> {
+    private T value;
+    private Tree<T> parent;
+    private Tree<T> left;
+    private Tree<T> right;
+
+    Tree(T value) {
+        this(value, null, null, null);
+    }
+
+    Tree(T value, Tree<T> parent, Tree<T> left, Tree<T> right) {
+        this.value = value;
+        this.parent = parent;
+        this.left = left;
+        this.right = right;
+    }
+
+    /**
+     * Add new element
+     * @param newValue -- new element
+     * @param parentVal -- value of parent
+     * @param isLeft -- is left or right child
+     * @return true if added
+     */
+    boolean add(T newValue, T parentVal, boolean isLeft) {
+        if (parentVal.equals(value)) {
+            if (isLeft) {
+                left = new Tree<T>(newValue, this, left, null);
+                if (left.left != null) {
+                    left.left.parent = left;
+                }
+            } else {
+                right = new Tree<T>(newValue, this, null, right);
+                if (right.right != null) {
+                    right.right.parent = right;
+                }
+            }
+            return true;
+        } else {
+            return (left != null && left.add(newValue, parentVal, isLeft))
+                    || (right != null && right.add(newValue, parentVal, isLeft));
+        }
+    }
+
+    /**
+     * Get list of ancestors for element
+     * @param element -- value of element
+     * @return -- list of ancestors
+     */
+    List<T> getAncestors(T element) {
+        if (value.equals(element)) {
+            return getParents();
+        } else {
+            List<T> leftRes = left == null ? new LinkedList<T>() : left.getAncestors(element);
+            List<T> rightRes = right == null ? new LinkedList<T>() : right.getAncestors(element);
+            if (leftRes.isEmpty()) {
+                return rightRes;
+            } else {
+                return leftRes;
+            }
+        }
+    }
+
+    /**
+     * Get parents for current element
+     * @return list of ancestors
+     */
+    private List<T> getParents() {
+        if (parent == null) {
+            return new LinkedList<T>();
+        } else {
+            List<T> res = parent.getParents();
+            res.add(0, parent.value);
+            return res;
+        }
+    }
+
+}
